3d3d927cf208d9cb68d4703868e5f98870090173
cesar <kyele936@gmail.com>
2015-03-07 18:55:17 -0600

    esqueleto bace de ui-route manejador de vistas

--- public/js/controllers/app_doctorado.js
+++ public/js/controllers/app_doctorado.js
@@ -1,31 +1,42 @@
-angular.module( 'appDoctorado' , [ "ui.router" ] )
-    .config(function($stateProvider, $urlRouterProvider){
-      
-      // For any unmatched url, send to /route1
-      $urlRouterProvider.otherwise("/route1")
-      
-      $stateProvider
-        .state('route1', {
-            url: "/route1",
-            templateUrl: "route1.html"
-        })
-          /*.state('route1.list', {
-              url: "/list",
-              templateUrl: "route1.list.html",
-              controller: function($scope){
-                $scope.items = ["A", "List", "Of", "Items"];
-              }
+var myapp = angular.module( 'appDoctorado' , [ "ui.router" ])
+      myapp.config(function( $stateProvider , $urlRouterProvider ){
+        
+        // For any unmatched url, send to /route1
+        $urlRouterProvider.otherwise( "/views/route1" )
+        
+        $stateProvider
+          .state( 'route1' , {
+              url: "/ruta 1", //nombre que aparecera en la url
+              templateUrl: "views/route1.html" //Direccion del archivo que sera llamado
           })
-          
-        .state('route2', {
-            url: "/route2",
-            templateUrl: "route2.html"
-        })
-          .state('route2.list', {
-              url: "/list",
-              templateUrl: "route2.list.html",
-              controller: function($scope){
-                $scope.things = ["A", "Set", "Of", "Things"];
-              }
-          })*/
-    });
+            .state( 'route1.list' , {
+                url: "/views/list",
+                templateUrl: "views/route1.list.html",
+                controller: function($scope){
+                  $scope.items = ["A", "List", "Of", "Items"];
+                }
+            })
+            
+          .state('route2', {
+              url: "/views/route2",
+              templateUrl: "views/route2.html"
+          })
+            .state('route2.list', {
+                url: "/views/list",
+                templateUrl: "views/route2.list.html",
+                controller: function($scope){
+                  $scope.things = ["A", "Set", "Of", "Things"];
+                }
+            })
+          .state('1', {
+            url: "/views/route2",
+            templateUrl: "views/route2.html"
+          })
+            .state('1.list', {
+                url: "/views/list",
+                templateUrl: "views/route2.list.html",
+                controller: function($scope){
+                  $scope.things = ["A", "Set", "Of", "Things"];
+                }
+            })
+      })
